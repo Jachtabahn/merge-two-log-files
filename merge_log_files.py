d6c1359baa9fa04fe5a78f3eaedbfc62a9f5bb5e
Dimitri Rusin <dimitri.rusin@smartfactory.de>
2021-01-22 12:35:26 +0100

    Create a Python function

--- merge_log_files.py
+++ merge_log_files.py
@@ -1,5 +1,15 @@
 import os
 
+def concatenate_lines(earlier_line, earlier_log_file, later_line, later_log_file):
+	print(earlier_line, end='')
+	for first_line in earlier_log_file:
+		print(first_line, end='')
+
+	print()
+	print(later_line, end='')
+	for second_line in later_log_file:
+		print(second_line, end='')
+
 first_log_path = "/home/dimitri/Documents/log/ubuntu.md"
 second_log_path = "/home/dimitri/Documents/log/windows.md"
 
@@ -16,20 +26,6 @@ first_mtime = os.stat(first_log_path).st_mtime
 second_mtime = os.stat(second_log_path).st_mtime
 
 if first_mtime <= second_mtime:
-	print(first_line, end='')
-	for first_line in first_log_file:
-		print(first_line, end='')
-
-	print()
-	print(second_line, end='')
-	for second_line in second_log_file:
-		print(second_line, end='')
+	concatenate_lines(first_line, first_log_file, second_line, second_log_file)
 else:
-	print(second_line, end='')
-	for second_line in second_log_file:
-		print(second_line, end='')
-
-	print()
-	print(first_line, end='')
-	for first_line in first_log_file:
-		print(first_line, end='')
+	concatenate_lines(second_line, second_log_file, first_line, first_log_file)
