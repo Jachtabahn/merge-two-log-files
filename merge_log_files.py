3b5c4623e0c8ecf454d0c37310970767d105cdfb
Dimitri Rusin <dimitri.rusin@smartfactory.de>
2021-01-22 12:27:23 +0100

    Consider the first uncommon lines and read the modification date of the two log files

--- merge_log_files.py
+++ merge_log_files.py
@@ -15,8 +15,21 @@ for first_line, second_line in zip(first_log_file, second_log_file):
 first_mtime = os.stat(first_log_path).st_mtime
 second_mtime = os.stat(second_log_path).st_mtime
 
-for first_line in first_log_file:
+if first_mtime <= second_mtime:
 	print(first_line, end='')
+	for first_line in first_log_file:
+		print(first_line, end='')
 
-for second_line in second_log_file:
+	print()
 	print(second_line, end='')
+	for second_line in second_log_file:
+		print(second_line, end='')
+else:
+	print(second_line, end='')
+	for second_line in second_log_file:
+		print(second_line, end='')
+
+	print()
+	print(first_line, end='')
+	for first_line in first_log_file:
+		print(first_line, end='')
