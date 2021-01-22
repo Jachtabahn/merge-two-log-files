f218cafa63076f8da53b7d124ab4670139f6e889
Dimitri Rusin <dimitri.rusin@smartfactory.de>
2021-01-22 12:16:36 +0100

    Merge two log files into one, while forgetting the first non-matching lines and not considering the modification dates of the two files

--- merge_log_files.py
+++ merge_log_files.py
@@ -1,4 +1,4 @@
-
+import os
 
 first_log_path = "/home/dimitri/Documents/log/ubuntu.md"
 second_log_path = "/home/dimitri/Documents/log/windows.md"
@@ -6,7 +6,17 @@ second_log_path = "/home/dimitri/Documents/log/windows.md"
 first_log_file = open(first_log_path)
 second_log_file = open(second_log_path)
 
-for line_number, (first_line, second_line) in enumerate(zip(first_log_file, second_log_file)):
-	if first_line != second_line:
-		print(line_number, first_line, second_line)
+for first_line, second_line in zip(first_log_file, second_log_file):
+	if first_line == second_line:
+		print(first_line, end='')
+	else:
 		break
+
+first_mtime = os.stat(first_log_path).st_mtime
+second_mtime = os.stat(second_log_path).st_mtime
+
+for first_line in first_log_file:
+	print(first_line, end='')
+
+for second_line in second_log_file:
+	print(second_line, end='')
