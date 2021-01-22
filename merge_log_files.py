257b1a0128fa13ae3264f32a0575726badd4eb29
Dimitri Rusin <dimitri.rusin@smartfactory.de>
2021-01-22 13:02:59 +0100

    Parse the paths of the two input to-be-merged log files

--- merge_log_files.py
+++ merge_log_files.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 def concatenate_lines(earlier_line, earlier_log_file, later_line, later_log_file):
@@ -10,11 +11,14 @@ def concatenate_lines(earlier_line, earlier_log_file, later_line, later_log_file
 	for second_line in later_log_file:
 		print(second_line, end='')
 
-first_log_path = "/home/dimitri/Documents/log/ubuntu.md"
-second_log_path = "/home/dimitri/Documents/log/windows.md"
+# Parse the two input paths to the two log files.
+parser = argparse.ArgumentParser(description='Merge two log files.')
+parser.add_argument('first_log_path', type=str, help='the path of the first log file to merge')
+parser.add_argument('second_log_path', type=str, help='the path of the second log file to merge')
+args = parser.parse_args()
 
-first_log_file = open(first_log_path)
-second_log_file = open(second_log_path)
+first_log_file = open(args.first_log_path)
+second_log_file = open(args.second_log_path)
 
 for first_line, second_line in zip(first_log_file, second_log_file):
 	if first_line == second_line:
@@ -22,8 +26,8 @@ for first_line, second_line in zip(first_log_file, second_log_file):
 	else:
 		break
 
-first_mtime = os.stat(first_log_path).st_mtime
-second_mtime = os.stat(second_log_path).st_mtime
+first_mtime = os.stat(args.first_log_path).st_mtime
+second_mtime = os.stat(args.second_log_path).st_mtime
 
 if first_mtime <= second_mtime:
 	concatenate_lines(first_line, first_log_file, second_line, second_log_file)
